7cf92cad43df47d30a22fc5ff1bbfd7f3632d80b
Jaymin Soni <jsoni@juiceworldwide.com>
2017-05-16 11:50:21 -0400

    Initial commit

---
+++ sourcetree.php
@@ -0,0 +1,11 @@
+learning version control
+
+Working in new direction
+
+Step 1 - learning commit and history of the files
+
+Step 2 - Added Branch: new-direction
+
+learning stashes
+
+Merged to master and started again editing new-direction
